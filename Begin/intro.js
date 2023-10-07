e0cc342bbbd29d723ef0d75a7d2e87aabc85a831
Dunker21 <143425751+Dunker21@users.noreply.github.com>
2023-10-07 05:40:42 +0000

    setup modifictions

--- Begin/intro.js
+++ Begin/intro.js
@@ -1,1 +1,12 @@
 console.log("Darkuma")
+const number= 32345
+let name= "Vinay"
+var password = "12345"
+
+console.log(number+" "+name+" "+password)
+
+console.log(typeof password)
+let newPassword= Number(password)
+console.log(typeof newPassword)
+console.table([number,name,password,newPassword])
+
