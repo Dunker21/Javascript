c99430dfb69ab589b4fc84d89ccc670886305725
Dunker21 <143425751+Dunker21@users.noreply.github.com>
2023-10-07 07:07:46 +0000

    comparison and operations

---
+++ Begin/comparison.js
@@ -0,0 +1,26 @@
+// console.log(2>1)
+// console.log(2<1)
+// console.log(2<=1)
+// console.log(2>=1)
+// console.log(2==1)
+// console.log(2!=1)
+
+// console.log("2">1) // true
+// console.log("04">1) //true
+
+// console.log(null >0 ) //false
+// console.log(null ==0 ) //false
+// console.log(null >=0 ) //true  // == and (=> =<) works differently in javascript
+
+// console.log(undefined == 0); //false
+// console.log(undefined != 0); //true
+// console.log(undefined > 0); //false
+// console.log(undefined < 0); //false
+// console.log(undefined >= 0); //false
+// console.log(undefined <= 0); //false
+
+
+console.log("2" == 2)  //true
+console.log("2" === 2) //false
+
+//===       //strictly equal to check data type also
---
+++ Begin/operation.js
@@ -0,0 +1,30 @@
+let a=3
+let b=5
+
+// console.log(a+b)
+// console.log(a-b)
+// console.log(a*b)
+// console.log(a**b)
+// console.log(a/b)
+// console.log(a%b)
+
+
+let per1 = "Hello there"
+let per2 = " I am vinay"
+// console.log(per1+per2)
+
+// console.log(1+3)          // 4
+// console.log("1"+"3")      //13
+// console.log("1"+3)         //13
+// console.log("1"+3+4)       //134
+// console.log(1+3+"4")       //44
+// console.log(1+"3"+4)       //134
+// console.log("2"+"3"-"4")       // 23 -4 = 19
+
+
+console.log(+true);
+console.log(+"");
+
+// link to study
+/* https://tc39.es/ecma262/multipage/abstract-operations.
+html#sec—type—conversion */
